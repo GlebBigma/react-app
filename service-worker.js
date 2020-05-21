acccb58f192570303f8fe983f189f5f8c3a7de31
Gleb Bigma <glebbigma@gmail.com>
2020-05-22 00:57:12 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.7cea810234676c77cf90e4541e639bfa.js"
+  "/react-app/precache-manifest.80e214c84b43657da2f4b22b6d277bc7.js"
 );
 
 self.addEventListener('message', (event) => {
